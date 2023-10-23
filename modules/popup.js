15dc0cb11394e8fa4e4e654a4cd927ec14db2b4e
Vitaly <vit.bogachev@gmail.com>
2023-10-23 11:51:15 +0300

    Separate button handlers

--- modules/popup.js
+++ modules/popup.js
@@ -6,42 +6,47 @@ const loginButton = document.querySelector('.login_button');
 const enterCodeButton = document.querySelector('.enter_code');
 const inputCode = document.getElementById('code');
 const closePopupButtons = document.querySelectorAll('.pop_up_close');
-
 const settingsPopup = document.querySelector('.pop_up_set');
 const authorizePopup = document.querySelector('.pop_up_authorize');
 const loginPopup = document.querySelector('.pop_up_login');
 
-
-settingsPopupButton.addEventListener('click', function(event) {
+const settingsButtonHandler = (event) => {
   event.preventDefault();
-  settingsPopup.classList.add('active');
-});
+  settingsPopup.classList.add('active');  
+};
 
-logoutPopupButton.addEventListener('click', function(event) {
+const logoutButtonHandler = (event) => {
   event.preventDefault();
   authorizePopup.classList.add('active');
-});
+};
 
-closePopupButtons.forEach((item) => {
-  item.addEventListener('click', () => {
-    settingsPopup.classList.remove('active');
-    authorizePopup.classList.remove('active');
-    loginPopup.classList.remove('active');
-  });
-});
-
-enterCodeButton.addEventListener('click', function(event) {
-  event.preventDefault();
+const closeButtonHandler = (event) => {
+  settingsPopup.classList.remove('active');
   authorizePopup.classList.remove('active');
+  loginPopup.classList.remove('active');
+};
 
-  loginPopup.classList.add('active');
-});
-
-loginButton.addEventListener('click', (event) => {
+const enterCodeButtonHandler = (event) => {
   event.preventDefault();
-  console.log(inputCode.value);
+  
+  authorizePopup.classList.remove('active');
+  loginPopup.classList.add('active');
+};
+
+const loginButtonHandler = (event) => {
+  event.preventDefault();
+  
   setCookie('code', inputCode.value, {secure: true, 'max-age': 360000});
   console.log(getCookie('code'));
+};
+
+settingsPopupButton.addEventListener('click', settingsButtonHandler);
+logoutPopupButton.addEventListener('click', logoutButtonHandler);
+enterCodeButton.addEventListener('click', enterCodeButtonHandler);
+loginButton.addEventListener('click', loginButtonHandler);
+
+closePopupButtons.forEach((item) => {
+  item.addEventListener('click', closeButtonHandler);
 });
 
 // setTimeout( () => { logoutPopup.classList.add('active') }, 1000)
