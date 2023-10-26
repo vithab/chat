dd741137689a337547772b8ca97465b41de232ac
Vitaly <vit.bogachev@gmail.com>
2023-10-26 12:05:52 +0300

    Add scroll

--- modules/const.js
+++ modules/const.js
@@ -1,8 +1,6 @@
-export const MESSAGES = [
- 
-]
-
+export const MESSAGES = [];
 export const CURRENT_USER_EMAIL = 'bogachevlab@gmail.com';
 export const myMessage = document.getElementById('my-message');
 export const companionMessage = document.getElementById('compaion-message');
 export const chatArea = document.querySelector('.chat-area');
+export const wrapper = document.querySelector('.wrapper');
--- modules/render.js
+++ modules/render.js
@@ -1,5 +1,5 @@
 import { addMessage, sortByField } from "./logic.js";
-import { MESSAGES, CURRENT_USER_EMAIL, myMessage, companionMessage, chatArea} from "./const.js";
+import { MESSAGES, CURRENT_USER_EMAIL, myMessage, companionMessage, chatArea, wrapper} from "./const.js";
 
 export function createMessageElement(username, text, email, date, id) {
   const templateRoot = document.createElement('div'); // создает новый HTML элемент
@@ -32,4 +32,37 @@ export function messagesRender(MESSAGES) {
     createMessageElement( message.username, message.text, message.email, message.date, message.id)
   }
   )
+};
+
+function checkPosition() {
+  // Нам потребуется знать высоту документа и высоту экрана:
+  const height = document.body.offsetHeight
+  const screenHeight = wrapper.innerHeight
+
+  // Они могут отличаться: если на странице много контента,
+  // высота документа будет больше высоты экрана (отсюда и скролл).
+
+  // Записываем, сколько пикселей пользователь уже проскроллил:
+  const scrolled = wrapper.scrollY
+
+  // Обозначим порог, по приближении к которому
+  // будем вызывать какое-то действие.
+  // В нашем случае — четверть экрана до конца страницы:
+  const threshold = height - screenHeight / 4
+
+  // Отслеживаем, где находится низ экрана относительно страницы:
+  const position = scrolled + screenHeight
+
+  if (position >= threshold) {
+    // Если мы пересекли полосу-порог, вызываем нужное действие.
+  }
 }
+
+wrapper.addEventListener('scroll', checkPosition);
+wrapper.addEventListener('resize', checkPosition)
+
+
+// chatArea.addEventListener('scroll', function() {
+//   const scrollPosition = chatArea.pageYOffset || document.documentElement.scrollTop;
+//   console.log('Текущая прокрутка страницы:', scrollPosition);
+// })
